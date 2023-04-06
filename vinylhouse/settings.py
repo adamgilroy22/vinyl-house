34556136afaf4b688179a3211cb4d0270c42bec6
adamgilroy22 <>
2023-04-06 17:29:42 +0000

    Move IDE from Gitpod to Codeanywhere

--- vinylhouse/settings.py
+++ vinylhouse/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['8000-adamgilroy22-vinyl-house-y91b8uuhua.us2.codeanyapp.com']
 
 
 # Application definition
